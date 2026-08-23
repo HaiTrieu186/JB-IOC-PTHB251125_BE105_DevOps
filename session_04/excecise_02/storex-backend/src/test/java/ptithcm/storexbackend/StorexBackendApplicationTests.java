f918013d6976845bc642eb8f741b9935721952d4
HaiTrieu186 <pnhtrieu186@gmail.com>
2026-08-23 20:24:10 +0700

    Hoan thanh BT02 (Kha) Session04

---
+++ session_04/excecise_02/storex-backend/src/test/java/ptithcm/storexbackend/StorexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ptithcm.storexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
